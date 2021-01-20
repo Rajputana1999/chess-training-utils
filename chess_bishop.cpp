48b86ca85e93454a493b1c5d305fcf4b62ec8578
Jatin Rajput <jatinraj1999007@gmail.com>
2021-01-21 01:13:20 +0530

    fixed chess_bishop

--- chess_bishop.cpp
+++ chess_bishop.cpp
@@ -18,16 +18,16 @@ int abs( int a ){
 class square{
 	public :
 		int rank, file;
-		square(int f=0, int r=0) : file(f), rank(r) {};
+		square(int f=1, int r=1) : file(f), rank(r) {};
 		string value(){
 			string val="";
-			val.push_back(char('a'+file));
-			val.push_back(char('1'+rank));
+			val.push_back(char('a'+file-1));
+			val.push_back(char('1'+rank-1));
 			return val;
 		}
 		
 		bool isvalid(){
-			return rank>=0 && rank<=7 && file>=0 && file<=7;
+			return rank>=1 && rank<=8 && file>=1 && file<=8;
 		}
 
 		int color(){
@@ -40,20 +40,21 @@ class square{
 		 	* find two squares in the same rank as the target square ( r1, r2 )
 		 	* if file difference b/w rx and target is odd => not reachable( different colors )
 		 	*/
-			if( color() ^ target.color() ) return {}; /// file difference if odd => NP		
+			if( color() ^ target.color() ) return {}; /// The colors are different
 			
 			if( abs(file-target.file) == abs(rank-target.rank) ) return {target};
 
 			int rankDiff = abs( rank - target.rank );
 			square horizontal1(file+rankDiff, target.rank), horizontal2(file-rankDiff, target.rank);
-
+			int dir = ( target.rank-rank ) >= 0 ? +1:-1;
+			dir  = ( (target.file-file)>0 ? dir:-dir);
 			// vector for 1st intersecting square
-			int direction1 = (horizontal1.file - target.file) > 0 ? +1: -1;
+			int direction1 = dir;
 			int magnitude1 = abs( target.file - horizontal1.file)/2;
 			int file1 = (target.file + horizontal1.file)/2;
 
 			// vector for 2nd intersecting square
-			int direction2 = (horizontal2.file - target.file) > 0 ? -1: +1;
+			int direction2 = (file==target.file ? dir:-dir);
 			int magnitude2 = abs( target.file - horizontal2.file)/2;
 			int file2 = (target.file + horizontal2.file)/2;
 
@@ -68,28 +69,32 @@ class square{
 int main(){
 	cout << " Enter the intersecting square for bishop (press 's' if on same diagonal) ( press 'n' if doesn't exist)\n";
 	
-	// generate two random squares
 	srand( time(0) );
-	square source(rand()%8, rand()%8), target(rand()%8, rand()%8);
-	
-	// input a string
-	cout << source.value() << " -> " << target.value() << " : ";
 	string mid;
-	getline(cin, mid);
-	
-	// check against correct value , empty string if NP
-	vector<square> answers = source.findIntersection(target);
-	if( answers.size() == 0 ) cout<<" Opposite Colors ";
-	else for( auto &i: answers) if(i.isvalid())cout<< i.value() << " ";
-	if( (answers.size()==0 && mid=="n")  
-		|| (answers.size() == 1 && mid=="s") 
-		|| (answers.size() > 1 && answers[0].isvalid() && mid==answers[0].value()) 
-		|| (answers.size() > 1 && answers[1].isvalid() && mid==answers[1].value()) 
-	){
-		cout << " Correct\n";
-	}
-	else {
-		cout<< "Not Correct\n";
+	while(1){
+		// generate two random squares
+		square source(1, 2), target(1, 8);
+ 		
+		// input a string
+		cout << " " << source.value() << " -> " << target.value() << " : ";
+		cin>>mid;
+		if(mid=="q")break;
+
+		// check against correct value , empty string if NP
+		vector<square> answers = source.findIntersection(target);
+		if( answers.size() == 0 ) cout<<" Opposite Colors ";
+		else cout<<" ";
+		for( auto &i: answers) if(i.isvalid())cout<< ":" <<i.value() << " ";
+		if( (answers.size()==0 && mid=="n")  
+			|| (answers.size() == 1 && mid=="s") 
+			|| (answers.size() > 1 && answers[0].isvalid() && mid==answers[0].value()) 
+			|| (answers.size() > 1 && answers[1].isvalid() && mid==answers[1].value()) 
+		){
+			cout << " Correct\n";
+		}
+		else {
+			cout<< " Not Correct\n";
+		}
 	}
 	return 0;
 }
